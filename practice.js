68f87e3266ff7fc00c6b758abd42f1dce09646bb
Tasmiah-coder <tasmiahtabassum13218@gmail.com>
2024-10-19 10:32:05 +0600

    problem-1 solved

---
+++ practice.js
@@ -0,0 +1,31 @@
+// PROBLEM-1:Manage a Todo list 
+
+let todoList = ["Do laundry", "Pay bills", "Walk the dog"];
+
+// step-1
+todoList.push ("Buy groceries")
+
+console.log(todoList);
+
+// step-2
+todoList.unshift("Clean room")
+
+console.log(todoList);
+
+// step-3
+todoList.pop()
+console.log(todoList);
+
+// step-4
+console.log(todoList.indexOf("Clean room"));
+
+todoList.shift()
+console.log(todoList);
+
+// step-5 
+console.log(todoList.length);
+
+
+
+
+
