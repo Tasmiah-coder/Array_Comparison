bd53fc081fb5cc19830398646f809b47ba382123
Tasmiah-coder <tasmiahtabassum13218@gmail.com>
2024-10-19 10:55:55 +0600

    problem-2 solved

--- practice.js
+++ practice.js
@@ -27,5 +27,26 @@ console.log(todoList.length);
 
 
 
+// PROBLEM-2:Guest list 
 
+let guests = ["Alice","Bob"];
+// step-1
+guests.push("Tanjil","Fahim","Tajrimin")
 
+console.log(guests);
+
+// step-2
+guests.shift()
+
+console.log(guests);
+
+// step-3
+guests.unshift("Ananna")
+
+console.log(guests);
+
+// step-4 
+console.log(guests.indexOf("John Doe"));
+
+// step-5 
+console.log(guests.length);
