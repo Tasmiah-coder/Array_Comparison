aca365a400e67ae084a4116138d2000b3d3af1ea
Tasmiah-coder <tasmiahtabassum13218@gmail.com>
2024-10-19 11:52:09 +0600

    problem-grade evaluation solved

--- comparison.js
+++ comparison.js
@@ -3,9 +3,21 @@
 let score = 85;
 
 if (score>=90){
-    console.log("Grade=A")
+    console.log("Grade:A");
 }
 
-if (90>=score>=80){
-    console.log("Grade=B")
+else if (score>=80 && score<90){
+    console.log("Grad:B");
+}
+
+else if (score>=70 && score<80){
+    console.log("Grade:C");
+}
+
+else if (score>=60 && score<70){
+    console.log("Grade:D");
+}
+
+else{
+    console.log("Grade:F");
 }
